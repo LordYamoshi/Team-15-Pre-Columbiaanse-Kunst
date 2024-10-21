9c067de32dfe307c45ec9175c9a486b642d4b745
Mikeysaur <86874503+Mikeysaur@users.noreply.github.com>
2024-10-21 16:23:08 +0200

    hele slechte code van een nieuw plaatje laden op distance

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -9,3 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = position.move_toward(player.position, delta * 2.5)
+
+	if position.distance_to(player.position) < 5:
+		$Sprite3D.texture = load("res://2D Assets/monsterhandopen.png")
