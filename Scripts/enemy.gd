71dba400a61d7b5c91294543c5db932f51aaf419
Mikeysaur <86874503+Mikeysaur@users.noreply.github.com>
2024-10-23 12:22:18 +0200

    direction based movement

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,11 +1,15 @@
 extends CharacterBody3D
 
 var player
+var direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_parent().player
+	#print(direction)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = position.move_toward(player.position, delta * 2.5)
+	direction = position.direction_to(player.position)
+	#position = position.move_toward(player.position, delta * 2.5)
+	position = position + direction * delta * 2.5
