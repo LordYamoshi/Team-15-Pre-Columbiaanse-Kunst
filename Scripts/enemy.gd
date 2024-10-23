8ea78dc488a5caad5611f7905ee73ba1e4abd319
Bashar <babbas107@gmail.com>
2024-10-23 14:00:31 +0200

    Enemy Pattern Part 1
    WIP

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,14 +2,20 @@ extends CharacterBody3D
 
 var player
 var direction
+var sway_amount = 50.0
+var sway_speed = 5.0
+var sway_timer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_parent().player
+	sway_timer = Timer.new()
+	add_child(sway_timer)
 	#print(direction)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var sway_offset = sin(sway_timer.time_left * sway_speed) * sway_amount
 	direction = position.direction_to(player.position)
-	#position = position.move_toward(player.position, delta * 2.5)
-	position = position + direction * delta * 2.5
+	position += direction * delta * 2.5
